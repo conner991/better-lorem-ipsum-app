ec442e57571a073cc88786bc0f2e5d9132a7ec12
Conner Fissell <connerfissell91@gmail.com>
2022-04-26 10:59:22 -0700

    changed integrate.yml, testing2

--- app.js
+++ app.js
@@ -35,3 +35,16 @@ form.addEventListener("submit", function (e) {
     result.innerHTML = tempText; 
   }
 });
+
+const dayOfTheWeek = (date = new Date()) => {
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+  return days[date.getDay()];
+}
+
+try {
+  document.getElementById('day').innerText = dayOfTheWeek();
+} catch(err) {}
+
+
+exports.dayOfTheWeek = dayOfTheWeek;
